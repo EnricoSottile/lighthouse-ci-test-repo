4a54f8e201995fb5b4847942a314ecfabbc7ccaa
Enrico Sottile <enrico.sottile@gmail.com>
2021-08-11 12:33:47 +0200

    bad practice

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Hello my name is Enrico</h1>
+  <h2>Let's do some bad practices</h1>
+  <img src="" alt="" />
+
+  <h1></h1>
+  <a href="#">going nowhere</a>
+
+
+  <img src="https://via.placeholder.com/2000x2000" alt="" style="width:100px; height:100px">
 </template>
 
 <script>
