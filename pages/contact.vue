bf3986610d2dac782b3c1a1c46792771dd95f354
Enrico Sottile <enrico.sottile@gmail.com>
2021-08-11 12:43:01 +0200

    add big js library for triggering JS improvements

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,16 @@
 <template>
-  <h2>Let's do some bad practices</h1>
-  <img src="" alt="" />
+  <div>
+    <script src="three.js"></script>
+    <h2>Let's do some bad practices</h2>
+    <img src="" alt="" />
 
-  <h1></h1>
-  <a href="#">going nowhere</a>
+    <h1></h1>
+    <a href="#">going nowhere</a>
 
 
-  <img src="https://via.placeholder.com/2000x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/2000x2000" alt="" style="width:100px; height:100px">
+  </div>
+
 </template>
 
 <script>
