ee2d5859008a85c3fc1d76c8bcce68c3d5995227
Enrico Sottile <enrico.sottile@gmail.com>
2021-08-12 09:22:19 +0200

    some more bad practices

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,7 +8,41 @@
     <a href="#">going nowhere</a>
 
 
+    <!-- UIkit CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/css/uikit.min.css" />
+
+    <!-- UIkit JS -->
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.7.2/dist/js/uikit-icons.min.js"></script>
+
+
+    <img src="https://via.placeholder.com/1000x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1100x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1200x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1300x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1400x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1500x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1600x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1700x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1800x2000" alt="" style="width:100px; height:100px">
+    <img src="https://via.placeholder.com/1900x2000" alt="" style="width:100px; height:100px">
     <img src="https://via.placeholder.com/2000x2000" alt="" style="width:100px; height:100px">
+
+    <script
+      src="https://code.jquery.com/jquery-1.12.4.min.js"
+      integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+      crossorigin="anonymous"></script>
+
+    <script
+      src="https://code.jquery.com/jquery-2.2.4.min.js"
+      integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+      crossorigin="anonymous"></script>
+
+    <script
+      src="https://code.jquery.com/jquery-3.6.0.min.js"
+      integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+      crossorigin="anonymous"></script>
+
   </div>
 
 </template>
